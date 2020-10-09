f9201373a4fcee4677189a81928e14d65907c52c
beeust <89119404+beeust@users.noreply.github.com>
2020-10-09 21:45:43 +0300

    First test

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MemoryToken is ERC721 {
-    constructor() public ERC721("Memory Token", "18") {}
+    constructor() public ERC721("Memory Token", "MEMORY") {}
 }
